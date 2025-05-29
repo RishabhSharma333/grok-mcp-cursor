3a3749d15ca68e18394af13470cf0ac98a296b09
Rishabh Sharma <rishabh78974@gmail.com>
2025-05-29 12:24:33 +0530

    Configured classes and usage of API key

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,75 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
-const server = new McpServer({
-    name: "grok-server",
-    version: "1.0.0",
-});
+
+
+const API_KEY = process.env.GROK_API_KEY;
+if (!API_KEY) {
+    throw new Error('[Error] XAI_API_KEY environment variable is required');
+}
+
+class GrokConfig {
+    private apiKey: string | undefined;
+    private baseUrl: string = 'https://api.x.ai/v1';
+    constructor(apikey: string) {
+        this.apiKey = API_KEY;
+    }
+}
+
+class GrokServer {
+    private server: McpServer;
+    // private grokConfig:GrokConfig;
+    constructor() {
+        this.server = new McpServer({
+            name: "grok-server",
+            version: "1.0.0",
+
+        },
+            {
+                capabilities: {
+                },
+            }
+        );
+        // this.grokConfig = new GrokConfig(API_KEY as string);
+    }
+
+    // Start receiving messages on stdin and sending messages on stdout
+    async run() {
+        const transport = new StdioServerTransport();
+        await this.server.connect(transport);
+    }
+}
+
+
 
 //tools let LLM take actions through your server.
 
-server.tool("grok-model",
-    "This returns summation of two numbers",
-    { a: z.number(), b: z.number() },
-    async ({ a, b }) => {
-        return {
-            content: [{ type: 'text', text: `This will return summation of two numbers ${a + b}` }]
-        }
-    }
-);
+// server.tool("grok-model",
+//     "This returns summation of two numbers",
+//     { a: z.number(), b: z.number() },
+//     async ({ a, b }) => {
+//         return {
+//             content: [{ type: 'text', text: `This will return summation of two numbers ${a + b}` }]
+//         }
+//     }
+// );
 
-server.tool(
-    "fetch-weather",
-    { city: z.string() },
-    async ({ city }) => {
-        const response = await fetch(`https://api.weather.com/${city}`);
-        const data = await response.text();
-        return {
-            content: [{ type: "text", text: data }]
-        };
-    }
-);
+// server.tool(
+//     "fetch-weather",
+//     "this will fethch weather details",
+//     { city: z.string() },
+//     async ({ city }) => {
+//         const response = await fetch(`https://api.weather.com/${city}`);
+//         const data = await response.text();
+//         return {
+//             content: [{ type: "text", text: data }]
+//         };
+//     }
+// );
+
+
 
 // resources are how you expose data to the LLMs, they provide data but do not do any significant conputation. It can be dynamic or static
 
 // prompts are reusable templates that help LLMs interact with your server 
 
-const transport = new StdioServerTransport();
-await server.connect(transport);
